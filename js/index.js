232ad7af940ecb565b6e825e9e129ac18cb36a5a
MarkUA <mark.d.nahorniak@gmail.com>
2024-09-22 12:33:53 +0300

    fix

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// const result = 5 + 5 + "5";
+// console.log(result)
+
+//-----------------------------------------------
+
+// const email = "mark.d.nahorniak@gmail.com"
+// const checkEmail = email.includes("@gmail.com")
+// console.log(checkEmail)
+
+//-----------------------------------------------
+
+// const My = "My "
+// const name = "name "
+// const is = "is "
+// const fullName = My + name + is + "Mark"
+// console.log(fullName)
+
+//-----------------------------------------------
+
+const userName = "Тетяна"
+const payment = "300грн"
+confirm(`Дякуємо, ${userName}! До сплати 25 гривень карта 5167 8032 4053 4896`)
+alert("Дякую що підтвердили платіж")
